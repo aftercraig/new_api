88f74b63287e798f4c47f671c6197d40eccbe54b
Aftercraig <pavelboritko@gmail.com>
2023-01-25 09:49:38 +0300

    new page

---
+++ view/contacts.js
@@ -0,0 +1,23 @@
+const aboutMe = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    </head>
+    <body>
+        <h1>About me</h1>
+        <ul>
+            <li>Name: Pavel</li>
+            <li>Age: 18</li>
+            <li>Preferred hobby: programming</li>
+            <li>City: Surgut</li>
+        </ul>
+    </body>
+    </html>
+`
+
+
+module.exports = aboutMe;
