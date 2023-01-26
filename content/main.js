a90499eb72c2e24f455553f0ac705c177834b7b1
aftercraig <pavelboritko@gmail.com>
2023-01-26 22:10:43 +0500

    +

---
+++ content/main.js
@@ -0,0 +1,17 @@
+const main = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is my first api</h1>
+    <p>Ничего особенного, просто главная страница</p>
+</body>
+</html>
+`
+
+module.exports = main;
