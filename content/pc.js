f79eb36fa2bad5b8ed1e34ccf27bb21332609d3a
aftercraig <pavelboritko@gmail.com>
2023-01-26 22:11:34 +0500

    +page about pc components

---
+++ content/pc.js
@@ -0,0 +1,26 @@
+const pcspec = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Комплектующие моего компьютера:</h1>
+    <ul>
+        <li>Процессор: Intel core I9 9990es 2.60GHz (boost 4.20GHz)</li>
+        <li>Охлаждение: Gamer Storm Assassin II</li>
+        <li>Видеокарта: Nvidia geforce GTX 1660Ti</li>
+        <li>Материнская плата: Asus ROG STRIX z370-H</li>
+        <li>Оперативная память: разносортные DDR4 2.40GHz (boost 4.00GHz)</li>
+        <li>Блок питания: Aerocock Sus 800W+</li>
+        <li>Корпус: Aerocool shard</li>
+        <li>Мониторы: 1)Philips PHL 243V7, 2)Пожилой офисник</li>
+    </ul>
+</body>
+</html>
+`
+
+module.exports = pcspec;
