95bbf19f995fb811fcefb4cc0d983548306acdd3
aftercraig <pavelboritko@gmail.com>
2023-01-26 22:10:18 +0500

    +page about me

---
+++ content/aboutme.js
@@ -0,0 +1,22 @@
+const aboutme = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Обо мне:</h1>
+    <ul>
+        <li>Имя: Pavel</li>
+        <li>Возраст: 18</li>
+        <li>Город: Surgut</li>
+        <li>Хобби: кодинг, сборка и ремонт комплектующих пк, компьютерные игры (например дбд, ксго), чтение (люблю энциклопедии по Великой Отечественной)</li>
+    </ul>
+</body>
+</html>
+`
+
+module.exports = aboutme;
