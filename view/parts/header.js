60ff629deb1b675be47d93f569e2296db31750b5
aftercraig <pavelboritko@gmail.com>
2023-02-26 19:31:58 +0500

    created header

---
+++ view/parts/header.js
@@ -0,0 +1,21 @@
+module.exports = `
+<header>
+    <nav>
+        <ul>
+            <li class="links-style">
+                <a href="/">Главная страница</a>
+            </li>
+            <li class="links-style">
+                <a href="games">Игры</a>
+            </li>
+            <li class="links-style">
+                <a href="pc">Характеристики</a>
+            </li>
+            <li class="links-style">
+                <a href="aboutme">Обо мне</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+`
+
